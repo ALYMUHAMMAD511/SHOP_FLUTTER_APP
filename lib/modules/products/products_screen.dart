ab0310eee36d363b97924e32f53ffd70b84bd2ae
alymu <alym7mdd@gmail.com>
2023-03-15 20:39:57 +0200

    Flutter Shop App

--- lib/modules/products/products_screen.dart
+++ lib/modules/products/products_screen.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:shop_app/cubit/cubit.dart';
 import 'package:shop_app/cubit/states.dart';
 import 'package:shop_app/models/home_model.dart';
+import 'package:shop_app/shared/styles/colors.dart';
 
 class ProductsScreen extends StatelessWidget {
   const ProductsScreen({Key? key}) : super(key: key);
@@ -25,28 +26,130 @@ class ProductsScreen extends StatelessWidget {
         );
   }
 
-  Widget productsBuilder(HomeModel model) => Column(
-    children: [
-      CarouselSlider(
-          items: model.data!.banners.map((e) => Image(
-            image: NetworkImage('${e.image}'),
-            width: double.infinity,
-            fit: BoxFit.cover,
-          ),).toList(),
-          options: CarouselOptions(
-            height: 250.0,
-            initialPage: 0,
-            viewportFraction: 1.0,
-            enableInfiniteScroll: true,
-            reverse: false,
-            autoPlay: true,
-            autoPlayInterval: const Duration(seconds: 3),
-            autoPlayAnimationDuration: const Duration(seconds: 1),
-            autoPlayCurve: Curves.fastOutSlowIn,
-            scrollDirection: Axis.horizontal,
+  Widget productsBuilder(HomeModel model) => SingleChildScrollView(
+    physics: const BouncingScrollPhysics(),
+    child: Column(
+      children: [
+        CarouselSlider(
+            items: model.data!.banners.map((e) => Image(
+              image: NetworkImage(e.image),
+              width: double.infinity,
+              fit: BoxFit.cover,
+            ),).toList(),
+            options: CarouselOptions(
+              height: 250.0,
+              initialPage: 0,
+              viewportFraction: 1.0,
+              enableInfiniteScroll: true,
+              reverse: false,
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 3),
+              autoPlayAnimationDuration: const Duration(seconds: 1),
+              autoPlayCurve: Curves.fastOutSlowIn,
+              scrollDirection: Axis.horizontal,
+            ),
+        ),
+        const SizedBox(
+          height: 12.0,
+        ),
+        Container(
+          color: Colors.grey[300],
+          child: GridView.count(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            crossAxisCount: 2,
+            mainAxisSpacing: 1.0,
+            crossAxisSpacing: 1.0,
+            childAspectRatio: 1 / 1.53,
+            children: List.generate(
+                model.data!.products.length,
+                    (index) => buildGridProduct(model.data!.products[index])
+            ),
 
           ),
-      ),
-    ],
+        ),
+
+      ],
+    ),
+  );
+
+  Widget buildGridProduct(ProductModel model) => Container(
+    color: Colors.white,
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Stack(
+          alignment: AlignmentDirectional.bottomStart,
+          children: [
+            Image(
+              image: NetworkImage(model.image),
+              width: double.infinity,
+              height: 200.0,
+            ),
+            if(model.discount != 0)
+              Container(
+              padding: const EdgeInsets.symmetric(horizontal: 6.0),
+              color: Colors.red,
+              child: const Text(
+                'DISCOUNT',
+                style: TextStyle(
+                  fontSize: 9.0,
+                  color: Colors.white
+                ),
+              ),
+            ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                model.name,
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 15.0,
+                  height: 1.3,
+                ),
+              ),
+              Row(
+                children: [
+                  Text(
+                    '${model.price.round()}',
+                    style: const TextStyle(
+                      fontSize: 13.0,
+                      color: defaultColor
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 6.0,
+                  ),
+                  if(model.discount != 0)
+                    Text(
+                    '${model.oldPrice.round()}',
+                    style: const TextStyle(
+                      fontSize: 11.0,
+                      color: Colors.grey,
+                      decoration: TextDecoration.lineThrough,
+                    ),
+                  ),
+                  const Spacer(),
+                  IconButton(
+                    padding: EdgeInsets.zero,
+                    onPressed: (){},
+                    icon: const Icon(
+                        Icons.favorite_border,
+                        size: 15.0,
+                      ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    ),
   );
 }
